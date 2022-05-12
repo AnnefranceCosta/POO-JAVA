6fb3600ac145ba91f5325944a2d55f0da289e354
AnnefranceCosta <73175477+AnnefranceCosta@users.noreply.github.com>
2022-05-12 20:17:25 -0300

    Add files via upload

---
+++ Situacao Aluno/src/Situacao.java
@@ -0,0 +1,14 @@
+public class Situacao {
+
+	public static void main(String[] args) {
+		boolean pagamento = true;
+		{
+			if (pagamento == true) {
+				System.out.println("Pagamento realizado com sucesso!!");
+			} else {
+				System.out.println("Pagamento NÃO realizado!!");
+			}
+
+		}
+	}
+}
